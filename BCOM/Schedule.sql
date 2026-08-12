d979a447a8d044d6a0442f20217cab8bac5b4687
TonyLe97 <letungquan246@gmail.com>
2026-08-12 13:44:39 +0700

    ub focus

--- BCOM/Schedule.sql
+++ BCOM/Schedule.sql
@@ -108,6 +108,7 @@ DATENAME(weekday, COALESCE(ROSTER_RAW2.[Date], TRANSFER_RAW.[LWD], TERMINATION_R
 COALESCE(TRANSFER_RAW.[Remarks], TERMINATION_RAW.[Termination Reason], RESIGNATION_RAW.[Resignation Primary Reason]) AS [Termination/Transfer],
 CASE 
     WHEN ROSTER_RAW2.[LOB] IN ('NL', 'ID4', 'HE4', 'XT4', 'EL', 'TR', 'KO', 'IT', 'CS', 'HU', 'FR', 'ZH', 'RU', 'PL', 'PT', 'NO', 'DA', 'DE', 'RO', 'BG', 'VI TRA', 'ES', 'SV') THEN 'Unbabel'
+	WHEN ROSTER_RAW2.[LOB] IN ('NL', 'IT', 'FR', 'ES') THEN 'Unbabel Focus'
 	WHEN ROSTER_RAW2.[LOB] IN ('FR CSP', 'ES CSP', 'IT CSP', 'DE CSP', 'VI TRA CSP', 'ZH CSP', 'PT CSP', 'NO CSP', 'HR CSP', 'EL CSP', 'PL CSP', 'CS CSP', 'SV CSP', 'UB CSP') THEN 'Unbabel CSP'
     WHEN ROSTER_RAW2.[LOB] IN ('ENCSP', 'EN CSP') THEN 'English CSP'
     WHEN ROSTER_RAW2.[LOB] = 'EN' THEN 'English'
